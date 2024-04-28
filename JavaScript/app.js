5de08135e3eda73c4ab2a1b488fb29fab0688d53
Andrey <1.andrey.p.1999@gmail.com>
2024-04-28 15:53:14 +0300

    first commit

---
+++ JavaScript/app.js
@@ -0,0 +1,33 @@
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+document.querySelectorAll("section").forEach(section => {
+    gsap.from(section, {
+        opacity: 0,
+        y: -10,
+        duration: 1,
+        scrollTrigger: {
+            trigger: section,
+            start: "top 60%",
+            end: "bottom 10%",
+            toggleActions: "play reverse play reverse",
+            // markers: true,
+            onEnter: () => {
+                gsap.from(section.querySelectorAll(".--animated_show"), {
+                    opacity: 0,
+                    duration: 0.5,
+                    y: -10,
+                    stagger: 0.2,
+
+                });
+            }
+        }
+    });
+});
+
+var tl = gsap.timeline({ defaults: { duration: 0.5, ease: "none" } });
+tl.from(".header__bgImage", { opacity: 0, duration: 0.5 });
+tl.from(".navigete__link", { opacity: 0, duration: .4, stagger: 0.1 });
+tl.from(".main-banner", { opacity: 0, duration: .4 });
+tl.from("main", { opacity: 0, duration: .4 });
